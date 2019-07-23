d4487073bd6de5a4c095e8ee9032fdd1be8611c3
gabriel_piza <gabyzonlb85@gmail.com>
2019-07-23 17:13:15 -0300

    Agrego las superclase y sub clases de animales

--- eco.py
+++ eco.py
@@ -1,4 +1,44 @@
 # Grupo ECO - WTPC 2019
 # Este es el script que simula un ecosistema con cazador(depredador) y presa
 
-Gabriel
+class Animal(object):
+    def __init__(self, velocidad, vision, energia, energia_maxima, plan):
+        self.velocidad = velocidad
+		self.vision = vision
+		self.energia = energia
+		self.energia_maxima = energia_maxima
+		self.plan = plan
+   
+    def observar(self,terreno,vision):
+        vecinos = terreno.informar_vecinos(vision)
+		
+
+    def modificar_energia(self,obj,nombre):
+        
+		pass
+		
+class Cazador(Animal)
+    def decidir(self, )
+        
+        pass
+
+    def ejecutar(self, )
+	    
+	    pass
+
+    def comer(self,)
+        
+        pass
+		
+class Presa(Animal)
+    def decidir(self)
+	    
+		pass
+		
+	def ejecutar(self)
+	    
+		pass
+		
+	def pastar(self)
+	    
+		pass
