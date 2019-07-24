09f2e47e31cb388e4f18315749e0d18a503fa801
ABertoni <andresibertoni@gmail.com>
2019-07-24 19:03:26 -0300

    Desarrollado un poco más el main. Ahora muestra el estado inicial con imshow.

--- eco_main.py
+++ eco_main.py
@@ -3,6 +3,10 @@
 
 ###-----------------------------------
 ### 1) Se importan librerías y clases
+## 1.1) Librerías
+import matplotlib.pyplot as plt
+import time
+## 1.2) Clases
 from c_presa import Presa
 from c_predador import Predador
 from sc_terreno import Terreno
@@ -10,23 +14,49 @@ from sc_terreno import Terreno
 if __name__ == "__main__":
 
     ### 2) Se establecen los parámetros de simulación
-        ## 2.1) Set de parámetros de terreno
-        ## 2.2) Set de parámetros para animales
-        ## 2.3) Set de parámetros temporales
+    ## 2.1) Set de parámetros de terreno
+    n_filas = 100
+    n_columnas = 100
+    ## 2.2) Set de parámetros para animales
+    # 2.2.1) Predadores
+    n_predadores = 5
+    velocidad_predador = 1
+    vision_predador = 10
+    energia_maxima_predador = 10
+    # 2.2.2) Presas
+    n_presas = 20
+    velocidad_presa = 1
+    vision_presa = 10
+    energia_maxima_presa = 10
+    ## 2.3) Set de parámetros temporales
+    pasos_temporales = 10
 
     ### 3) Se construye un estado inicial
-    ### 3.1) Se crean objetos terreno y animales
-    terreno = Terreno(10,10)
-    lobo = Predador(1,1,1)
-    ### 3.2) Se insertan animales al terreno
-    posicion_random = terreno.generar_posicion_random()
-    terreno.insertar(lobo, posicion_random)
-    ### 3.3) Se guarda y/o visualiza el estado inicial
+    ## 3.1) Se crea el objeto terreno
+    terreno = Terreno(n_filas,n_columnas)
+    ## 3.2) Se crean e insertan animales al terreno con una posición random
+    # 3.2.1) Predadores
+    for i in range(n_predadores):
+        predador = Predador(velocidad_predador, vision_predador, energia_maxima_predador)
+        posicion_random = terreno.generar_posicion_random()
+        terreno.insertar(predador, posicion_random)
+    # 3.2.2) Presas
+    for j in range(n_presas):
+        presa = Presa(velocidad_presa, vision_presa, energia_maxima_presa)
+        posicion_random = terreno.generar_posicion_random()
+        terreno.insertar(presa, posicion_random)    
+    ### 3.3) Se visualiza el estado inicial
+    #plt.ion()
+    plt.imshow(terreno.visualizar())
+    plt.show()
 
     ### 4) Se ejecuta la simulación
-    lobo.decidir(terreno)
-    print(terreno.visualizar())
-
+"""	for paso in range(pasos_temporales):
+		posicion_random = terreno.generar_posicion_random()
+		terreno.insertar(oveja, posicion_random)
+		plt.imshow(terreno.visualizar())
+		plt.show(block=True)
+"""
     ### 4.1) Todos los animales deciden un plan de acción
     ### 4.2) Todos los animales ejecutan su plan
         ## 4.2.1) Ejecutan los predadores
